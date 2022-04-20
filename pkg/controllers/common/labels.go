b9f4290cdb83be5a1d2130ca6e1ec73114155dbf
Arvind Iyengar <arvind.iyengar@rancher.com>
2022-04-20 11:21:38 -0700

    Restructure common constants to two files for labels and formats

--- pkg/controllers/common/labels.go
+++ pkg/controllers/common/labels.go
@@ -19,8 +19,4 @@ const (
 	// HelmProjectOperatorDashboardValuesConfigMapLabel is a label that identifies a ConfigMap that should be merged into status.dashboardValues when available
 	// The value of this label will be the release name of the Helm chart, which will be used to identify which ProjectHelmChart's status needs to be updated.
 	HelmProjectOperatorDashboardValuesConfigMapLabel = "helm.cattle.io/dashboard-values-configmap"
-	// ProjectRegistrationNamespaceFmt is the format used in order to create project registration namespaces if ProjectLabel is provided
-	// If SystemProjectLabel is also provided, the project release namespace will be this namespace with `-<ReleaseName>` suffixed, where
-	// ReleaseName is provided by the Project Operator that implements Helm Project Operator
-	ProjectRegistrationNamespaceFmt = "cattle-project-%s"
 )
