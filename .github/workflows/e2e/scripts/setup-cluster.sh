5b888776d3bec596c7889336cd2013990275fe56
Alexandre Lamarre <alexandre.lamarre@suse.com>
2024-05-14 15:27:20 -0400

    correctly import image
    Signed-off-by: Alexandre Lamarre <alexandre.lamarre@suse.com>

--- .github/workflows/e2e/scripts/setup-cluster.sh
+++ .github/workflows/e2e/scripts/setup-cluster.sh
@@ -65,6 +65,5 @@ kubectl cluster-info --context k3d-${CLUSTER_NAME}
 kubectl config use-context k3d-${CLUSTER_NAME}
 kubectl get nodes -o wide
 
-IMAGE=${REPO}/backup-restore-operator:${TAG}
-
+echo "Importing image ${IMAGE}"
 k3d image import ${IMAGE} -c $CLUSTER_NAME
