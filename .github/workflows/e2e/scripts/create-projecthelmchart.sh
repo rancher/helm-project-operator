56cf70d8cb726e671968160ef7f266d87013543f
Dan Pock <self@danpock.me>
2024-09-10 20:45:31 -0400

    correct last reference to old name

--- .github/workflows/e2e/scripts/create-projecthelmchart.sh
+++ .github/workflows/e2e/scripts/create-projecthelmchart.sh
@@ -21,7 +21,7 @@ if ! kubectl get -n cattle-helm-system job/helm-install-project-operator-example
     exit 1
 fi
 
-if ! kubectl wait --for=condition=complete --timeout="${KUBECTL_WAIT_TIMEOUT}" -n cattle-helm-system job/helm-install-project-project-operator-example-dummy; then
+if ! kubectl wait --for=condition=complete --timeout="${KUBECTL_WAIT_TIMEOUT}" -n cattle-helm-system job/helm-install-project-operator-example-chart-dummy; then
     echo "ERROR: Helm Install Job for Example Chart never completed after ${KUBECTL_WAIT_TIMEOUT} seconds"
     kubectl logs job/helm-install-project-operator-example-chart-dummy -n cattle-helm-system
     exit 1
